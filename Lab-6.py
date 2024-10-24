551809cc6aa29ed339da13a208fcd21f0d913ee9
humza-noor <humza.noor@ufl.edu>
2024-10-23 22:04:00 -0400

    Update Lab-6.py

--- Lab-6.py
+++ Lab-6.py
@@ -9,6 +9,14 @@ def encode(pre_password):
         encoded_password += str(item)
     return encoded_password
 
+def decode(pre_password):
+    decoded_password = ""
+    for digit in pre_password:
+        decoded_digit = (int(digit) - 3) % 10  # Reverse the shift by 3
+        decoded_password += str(decoded_digit)
+    return decoded_password
+
+
 if __name__ == '__main__':
     encoded_pw = ''
     quit = False
@@ -17,14 +25,26 @@ if __name__ == '__main__':
         print('1. Encode \n 2. Decode \n 3. Quit')
         option = int(input('Please enter an option: '))
         if option == 1:
-            pre_encoded_pw = input('Please enter your password to encode: ')
-            encoded_pw = encode(pre_encoded_pw)
-            print('Your password has been encoded and stored!')
-            print()
-        elif option == 2:
-            pass
-        elif option == 3:
-            quit = True
+            pre_encoded_pw = input('Please enter your password to encode (8 digits): ')
+            if len(pre_encoded_pw) == 8 and pre_encoded_pw.isdigit():
+                encoded_pw = encode(pre_encoded_pw)
+                print(f'Your password has been encoded and stored as: {encoded_pw}')
+                print()
+            else:
+                print("Invalid input. Please enter an 8-digit password.")
 
+        elif option == 2:
+            if encoded_pw:
+                decoded_pw = decode(encoded_pw)
+                print(f'The encoded password is {encoded_pw}, and the original password is {decoded_pw}.')
+            else:
+                print("No encoded password found. Please encode a password first.")
+
+        elif option == 3:
+            quit_program = True
+            print("Goodbye!")
+
+        else:
+            print("Invalid option. Please choose 1, 2, or 3.")
 
 
